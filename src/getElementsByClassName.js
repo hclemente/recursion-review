dbd7bbc1327325b885ce0d32299bc1ac1956c224
TimothyAkana <TimothyAkana@gmail.com>
2020-10-05 16:35:43 -0700

    Passing tests for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,30 +5,37 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
-
-  var checkClassname = function () {
-    //
-
-  };
-
-  var checkChildren = function () {
-
-  };
-
-  // create results array
-  var checkElement = function () {
-
-    // Check the current element if it has the classname
-    // if it matches, add it to matchingElements array
-    // Check current element if it has child elements
-    // for each child element, call recursive function
-
-  };
-
-
-
   var matchingElements = [];
 
+  // Checks object for matching className
+  // if present adds the object to the array
+  var checkClassname = function (object) {
+    var classes = object.classList;
+    for (var key in classes) {
+      if (classes[key] === className) {
+        matchingElements.push(object);
+        break;
+      }
+    }
+  };
+
+  // iterate over the childNodes of the htmlElement
+  // recursively calls checkElement for each childNode
+  var checkChildren = function (htmlElement) {
+    var children = htmlElement.childNodes;
+    for (var i = 0; i < children.length; i++) {
+      checkElement(children[i]);
+    }
+  };
+
+  //Checks element for matching class and for childnodes
+  var checkElement = function (object) {
+    checkClassname(object);
+    checkChildren(object);
+  };
+
+  checkElement(document.body);
+
   // return results array
-  return;
+  return matchingElements;
 };
