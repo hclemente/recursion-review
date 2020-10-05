66554cba23be6883e4414d3f1d588e397c8d1643
TimothyAkana <TimothyAkana@gmail.com>
2020-10-05 12:40:35 -0700

    Started Pseudocode for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var checkClassname = function () {
+    //
+
+  };
+
+  var checkChildren = function () {
+
+  };
+
+  // create results array
+  var checkElement = function () {
+
+    // Check the current element if it has the classname
+    // if it matches, add it to matchingElements array
+    // Check current element if it has child elements
+    // for each child element, call recursive function
+
+  };
+
+
+
+  var matchingElements = [];
+
+  // return results array
+  return;
 };
