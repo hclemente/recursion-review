3e12e443bd640b09bc22e5b16833b6d57f869cf0
TimothyAkana <TimothyAkana@gmail.com>
2020-10-05 17:17:19 -0700

    Solved String, Number, Boolean, Null cases for Stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,24 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  if (typeof obj === 'string') {
+    return `"${obj}"`;
+  }
+  if (Array.isArray(obj)) {
+    //create a string
+    var arrayString = '[';
+    //iterate over array
+    for (var i = 0; i < obj.length; i++) {
+      //call stringify in each iteration
+      arrayString += stringifyJSON(obj[i]);
+      //TODO: figure out middle of array;
+    }
+    return arrayString + ']';
+  }
 };
